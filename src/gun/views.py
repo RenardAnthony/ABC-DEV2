fc477c8204c75d255139183433838d52855a2f87
Mr.Foxit <renard.anthony@outlook.fr>
2024-12-17 22:03:35 +0100

    add gitignor

--- src/gun/views.py
+++ src/gun/views.py
@@ -145,14 +145,20 @@ def calcul_puissance(request):
 
     if request.method == 'POST':
         try:
+            # Récupération des données utilisateur
             grammage_bille = float(request.POST.get('grammage_bille').replace(',', '.'))
             fps_mesures = float(request.POST.get('fps').replace(',', '.'))
 
+            # Conversion des unités
+            masse_kg = grammage_bille / 1000  # Conversion en kilogrammes
+            vitesse_ms = fps_mesures * 0.3048  # Conversion FPS en m/s
+
             # Calcul de la puissance en joules
-            puissance_joules = (fps_mesures ** 2) * grammage_bille / 2000
+            puissance_joules = (1 / 2) * masse_kg * (vitesse_ms ** 2)
             puissance_joules = round(puissance_joules, 2)
 
             return JsonResponse({'success': True, 'puissance_joules': puissance_joules})
+
         except (ValueError, KeyError):
             return JsonResponse({'success': False, 'message': "Valeurs invalides pour le calcul."})
 
